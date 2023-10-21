82d94c999a014592297bf7cde13fc1b30349cb47
paschal533 <okwuosahpaschal@gmail.com>
2023-10-21 20:58:49 +0100

    update commit

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ const nextConfig = {
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
